0bb021dcd3aaefb3a4dcde89d28516cb71704203
x-lin <x.lin@gmx.net>
2016-01-30 23:39:28 +0100

    initial setup + added bibo

---
+++ src/main/java/at/ac/tuwien/ifs/ontos/bibo/Bibo.java
@@ -0,0 +1,273 @@
+package at.ac.tuwien.ifs.ontos.bibo;
+
+/**
+ * Contains URIs for the bibo vocabulary (see: http://bibliontology.com/). Created by schemagen and customized.
+ * Based on Bibo version 1.3
+ *
+ * @author Xiashuo Lin
+ */
+public class Bibo {
+	/** <p>The namespace of the vocabulary as a string</p> */
+	public static final String NS = "http://purl.org/ontology/bibo/";
+
+	/** <p>The ontology's owl:versionInfo as a string</p> */
+	public static final String VERSION_INFO = "http://purl.org/ontology/bibo/1.3/";
+
+	/** <p>A scholarly academic article, typically published in a journal.</p> */
+	public static final String AcademicArticle = "http://purl.org/ontology/bibo/AcademicArticle";
+
+	/** <p>A written composition in prose, usually nonfiction, on a specific topic, forming
+	 *  an independent part of a book or other publication, as a newspaper or magazine.</p>
+	 */
+	public static final String Article = "http://purl.org/ontology/bibo/Article";
+
+	/** <p>An audio document; aka record.</p> */
+	public static final String AudioDocument = "http://purl.org/ontology/bibo/AudioDocument";
+
+	/** <p>An audio-visual document; film, video, and so forth.</p> */
+	public static final String AudioVisualDocument = "http://purl.org/ontology/bibo/AudioVisualDocument";
+
+	/** <p>Draft legislation presented for discussion to a legal body.</p> */
+	public static final String Bill = "http://purl.org/ontology/bibo/Bill";
+
+	/** <p>A written or printed work of fiction or nonfiction, usually on sheets of paper
+	 *  fastened or bound together within covers.</p>
+	 */
+	public static final String Book = "http://purl.org/ontology/bibo/Book";
+
+	/** <p>A section of a book.</p> */
+	public static final String BookSection = "http://purl.org/ontology/bibo/BookSection";
+
+	/** <p>A written argument submitted to a court.</p> */
+	public static final String Brief = "http://purl.org/ontology/bibo/Brief";
+
+	/** <p>A chapter of a book.</p> */
+	public static final String Chapter = "http://purl.org/ontology/bibo/Chapter";
+
+	/** <p>A collection of statutes.</p> */
+	public static final String Code = "http://purl.org/ontology/bibo/Code";
+
+	/** <p>A document that simultaneously contains other documents.</p> */
+	public static final String CollectedDocument = "http://purl.org/ontology/bibo/CollectedDocument";
+
+	/** <p>A collection of Documents or Collections</p> */
+	public static final String Collection = "http://purl.org/ontology/bibo/Collection";
+
+	/** <p>A meeting for consultation or discussion.</p> */
+	public static final String Conference = "http://purl.org/ontology/bibo/Conference";
+
+	/** <p>A collection of legal cases.</p> */
+	public static final String CourtReporter = "http://purl.org/ontology/bibo/CourtReporter";
+
+	/** <p>A document (noun) is a bounded physical representation of body of information
+	 *  designed with the capacity (and usually intent) to communicate. A document
+	 *  may manifest symbolic, diagrammatic or sensory-representational information.</p>
+	 */
+	public static final String Document = "http://purl.org/ontology/bibo/Document";
+
+	/** <p>a distinct part of a larger document or collected document.</p> */
+	public static final String DocumentPart = "http://purl.org/ontology/bibo/DocumentPart";
+
+	/** <p>The status of the publication of a document.</p> */
+	public static final String DocumentStatus = "http://purl.org/ontology/bibo/DocumentStatus";
+
+	/** <p>An edited book.</p> */
+	public static final String EditedBook = "http://purl.org/ontology/bibo/EditedBook";
+
+	/** <p>A written communication addressed to a person or organization and transmitted
+	 *  electronically.</p>
+	 */
+	public static final String Email = "http://purl.org/ontology/bibo/Email";
+
+	public static final String Event = "http://purl.org/ontology/bibo/Event";
+
+	/** <p>A passage selected from a larger work.</p> */
+	public static final String Excerpt = "http://purl.org/ontology/bibo/Excerpt";
+
+	/** <p>aka movie.</p> */
+	public static final String Film = "http://purl.org/ontology/bibo/Film";
+
+	/** <p>An instance or a session in which testimony and arguments are presented, esp.
+	 *  before an official, as a judge in a lawsuit.</p>
+	 */
+	public static final String Hearing = "http://purl.org/ontology/bibo/Hearing";
+
+	/** <p>A document that presents visual or diagrammatic information.</p> */
+	public static final String Image = "http://purl.org/ontology/bibo/Image";
+
+	/** <p>A formalized discussion between two or more people.</p> */
+	public static final String Interview = "http://purl.org/ontology/bibo/Interview";
+
+	/** <p>something that is printed or published and distributed, esp. a given number
+	 *  of a periodical</p>
+	 */
+	public static final String Issue = "http://purl.org/ontology/bibo/Issue";
+
+	/** <p>A periodical of scholarly journal Articles.</p> */
+	public static final String Journal = "http://purl.org/ontology/bibo/Journal";
+
+	/** <p>A document accompanying a legal case.</p> */
+	public static final String LegalCaseDocument = "http://purl.org/ontology/bibo/LegalCaseDocument";
+
+	/** <p>A document containing an authoritative determination (as a decree or judgment)
+	 *  made after consideration of facts or law.</p>
+	 */
+	public static final String LegalDecision = "http://purl.org/ontology/bibo/LegalDecision";
+
+	/** <p>A legal document; for example, a court decision, a brief, and so forth.</p> */
+	public static final String LegalDocument = "http://purl.org/ontology/bibo/LegalDocument";
+
+	/** <p>A legal document proposing or enacting a law or a group of laws.</p> */
+	public static final String Legislation = "http://purl.org/ontology/bibo/Legislation";
+
+	/** <p>A written or printed communication addressed to a person or organization and
+	 *  usually transmitted by mail.</p>
+	 */
+	public static final String Letter = "http://purl.org/ontology/bibo/Letter";
+
+	/** <p>A periodical of magazine Articles. A magazine is a publication that is issued
+	 *  periodically, usually bound in a paper cover, and typically contains essays,
+	 *  stories, poems, etc., by many writers, and often photographs and drawings,
+	 *  frequently specializing in a particular subject or area, as hobbies, news,
+	 *  or sports.</p>
+	 */
+	public static final String Magazine = "http://purl.org/ontology/bibo/Magazine";
+
+	/** <p>A small reference book, especially one giving instructions.</p> */
+	public static final String Manual = "http://purl.org/ontology/bibo/Manual";
+
+	/** <p>An unpublished Document, which may also be submitted to a publisher for publication.</p> */
+	public static final String Manuscript = "http://purl.org/ontology/bibo/Manuscript";
+
+	/** <p>A graphical depiction of geographic features.</p> */
+	public static final String Map = "http://purl.org/ontology/bibo/Map";
+
+	/** <p>A loose, thematic, collection of Documents, often Books.</p> */
+	public static final String MultiVolumeBook = "http://purl.org/ontology/bibo/MultiVolumeBook";
+
+	/** <p>A periodical of documents, usually issued daily or weekly, containing current
+	 *  news, editorials, feature articles, and usually advertising.</p>
+	 */
+	public static final String Newspaper = "http://purl.org/ontology/bibo/Newspaper";
+
+	/** <p>Notes or annotations about a resource.</p> */
+	public static final String Note = "http://purl.org/ontology/bibo/Note";
+
+	/** <p>A document describing the exclusive right granted by a government to an inventor
+	 *  to manufacture, use, or sell an invention for a certain number of years.</p>
+	 */
+	public static final String Patent = "http://purl.org/ontology/bibo/Patent";
+
+	/** <p>A public performance.</p> */
+	public static final String Performance = "http://purl.org/ontology/bibo/Performance";
+
+	/** <p>A group of related documents issued at regular intervals.</p> */
+	public static final String Periodical = "http://purl.org/ontology/bibo/Periodical";
+
+	/** <p>A communication between an agent and one or more specific recipients.</p> */
+	public static final String PersonalCommunication = "http://purl.org/ontology/bibo/PersonalCommunication";
+
+	/** <p>A personal communication manifested in some document.</p> */
+	public static final String PersonalCommunicationDocument = "http://purl.org/ontology/bibo/PersonalCommunicationDocument";
+
+	/** <p>A compilation of documents published from an event, such as a conference.</p> */
+	public static final String Proceedings = "http://purl.org/ontology/bibo/Proceedings";
+
+	/** <p>An excerpted collection of words.</p> */
+	public static final String Quote = "http://purl.org/ontology/bibo/Quote";
+
+	/** <p>A document that presents authoritative reference information, such as a dictionary
+	 *  or encylopedia .</p>
+	 */
+	public static final String ReferenceSource = "http://purl.org/ontology/bibo/ReferenceSource";
+
+	/** <p>A document describing an account or statement describing in detail an event,
+	 *  situation, or the like, usually as the result of observation, inquiry, etc..</p>
+	 */
+	public static final String Report = "http://purl.org/ontology/bibo/Report";
+
+	/** <p>A loose, thematic, collection of Documents, often Books.</p> */
+	public static final String Series = "http://purl.org/ontology/bibo/Series";
+
+	/** <p>A slide in a slideshow</p> */
+	public static final String Slide = "http://purl.org/ontology/bibo/Slide";
+
+	/** <p>A presentation of a series of slides, usually presented in front of an audience
+	 *  with written text and images.</p>
+	 */
+	public static final String Slideshow = "http://purl.org/ontology/bibo/Slideshow";
+
+	/** <p>A document describing a standard</p> */
+	public static final String Standard = "http://purl.org/ontology/bibo/Standard";
+
+	/** <p>A bill enacted into law.</p> */
+	public static final String Statute = "http://purl.org/ontology/bibo/Statute";
+
+	/** <p>A document created to summarize research findings associated with the completion
+	 *  of an academic degree.</p>
+	 */
+	public static final String Thesis = "http://purl.org/ontology/bibo/Thesis";
+
+	/** <p>The academic degree of a Thesis</p> */
+	public static final String ThesisDegree = "http://purl.org/ontology/bibo/ThesisDegree";
+
+	/** <p>A web page is an online document available (at least initially) on the world
+	 *  wide web. A web page is written first and foremost to appear on the web, as
+	 *  distinct from other online resources such as books, manuscripts or audio documents
+	 *  which use the web primarily as a distribution mechanism alongside other more
+	 *  traditional methods such as print.</p>
+	 */
+	public static final String Webpage = "http://purl.org/ontology/bibo/Webpage";
+
+	/** <p>A group of Webpages accessible on the Web.</p> */
+	public static final String Website = "http://purl.org/ontology/bibo/Website";
+
+	/** <p>A seminar, discussion group, or the like, that emphasizes zxchange of ideas
+	 *  and the demonstration and application of techniques, skills, etc.</p>
+	 */
+	public static final String Workshop = "http://purl.org/ontology/bibo/Workshop";
+
+	public static final String bdarcus = "http://purl.org/ontology/bibo/bdarcus";
+
+	/** <p>masters degree in arts</p> */
+	public static final String ma = "http://purl.org/ontology/bibo/degrees/ma";
+
+	/** <p>masters degree in science</p> */
+	public static final String ms = "http://purl.org/ontology/bibo/degrees/ms";
+
+	/** <p>PhD degree</p> */
+	public static final String phd = "http://purl.org/ontology/bibo/degrees/phd";
+
+	public static final String fgiasson = "http://purl.org/ontology/bibo/fgiasson";
+
+	/** <p>Accepted for publication after peer reviewing.</p> */
+	public static final String accepted = "http://purl.org/ontology/bibo/status/accepted";
+
+	/** <p>Document drafted</p> */
+	public static final String draft = "http://purl.org/ontology/bibo/status/draft";
+
+	/** <p>Document to be published</p> */
+	public static final String forthcoming = "http://purl.org/ontology/bibo/status/forthcoming";
+
+	/** <p>Legal document</p> */
+	public static final String legal = "http://purl.org/ontology/bibo/status/legal";
+
+	/** <p>A document that is not peer reviewed</p> */
+	public static final String nonPeerReviewed = "http://purl.org/ontology/bibo/status/nonPeerReviewed";
+
+	/** <p>The process by which articles are chosen to be included in a refereed journal.
+	 *  An editorial board consisting of experts in the same field as the author review
+	 *  the article and decide if it is authoritative enough for publication.</p>
+	 */
+	public static final String peerReviewed = "http://purl.org/ontology/bibo/status/peerReviewed";
+
+	/** <p>Published document</p> */
+	public static final String published = "http://purl.org/ontology/bibo/status/published";
+
+	/** <p>Rejected for publication after peer reviewing.</p> */
+	public static final String rejected = "http://purl.org/ontology/bibo/status/rejected";
+
+	/** <p>Unpublished document</p> */
+	public static final String unpublished = "http://purl.org/ontology/bibo/status/unpublished";
+
+}
